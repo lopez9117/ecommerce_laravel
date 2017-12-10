a43e358bef5271788fde5db7561759669ff5a6d6
lopez9117 <lu.lopez@misena.edu.co>
2017-12-10 01:26:29 -0500

    Modificando modelo carrito de comprar para conocer estado y contar productos que posee

--- app/ShoopingCart.php
+++ app/ShoopingCart.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ShoopingCart extends Model
 {
     //
+    protected $fillable = ["status"];
 
 public function inShoppingCarts()
 {
@@ -23,6 +24,34 @@ public function productsSize(){
 
 	return $this->products()->count();
 }
+
+		//Buscar o crear un carrito de compra
+public static function findOrCreateBySessionID($shopping_cart_id){
+	if ($shopping_cart_id) {
+		//Buscar el carrito de compra
+		return ShoopingCart::findBySession($shopping_cart_id);
+
+	}else
+	{
+		//Crear un carrito de compra
+			return ShoopingCart::createWithoutSession();
+	}
+
+	//Metodo para buscar el carrito de compra
+	public static function findBySession($shopping_cart_id){
+
+			return ShoopingCart::find($shopping_cart_id);
+	}
+
+	//metodo para crear carrito si este no existe
+	public static function createWithoutSession(){
+
+		return ShoopingCart::create([
+			"status" =>"imcompleted"	
+		]);
+	}
+
+} 
 
 
 
